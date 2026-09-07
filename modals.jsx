3be5d77cfcdb847b8b33f776602098f588dcd335
TrigmaticEngineeredForGrowth <trigmatic07@gmail.com>
2026-09-07 22:21:20 +0530

    Update Puja prices and names

--- modals.jsx
+++ modals.jsx
@@ -646,13 +646,13 @@ const VISHESHA_PUJAS = [
   { name: 'Veerabhadra Puja', price: 1111 },
   { name: 'Kaala Bhairava Puja', price: 1111 },
   { name: 'Ganesha Puja', price: 1111 },
-  { name: 'Sahasra Bilvarchana Puja', price: 1116 },
+  { name: 'Sahasra Bilvarchana Puja', price: 10008 },
   { name: 'Graha Dosha Parihara Puja', price: 2116 },
   { name: 'Shani Tailabhishekam', price: 2116 },
   { name: 'Vastra Samarpana Seva (Shiva-Shakti)', price: 2499 },
   { name: 'Shiva-Shakti Aaradhana Puja', price: 5116 },
-  { name: 'Birth Anniversary Seva with Rudrabhishekam', price: 5116 },
-  { name: 'Vivaha Anniversary Seva with Rudrabhishekam', price: 5116 },
+  { name: 'Birth Anniversary Seva with Rudrabhishekam-panchalingas', price: 5116 },
+  { name: 'Vivaha Anniversary Seva with Rudrabhishekam-Panchalingas', price: 5116 },
   { name: 'Navagrha Puja', price: 5116 },
 ];
 
